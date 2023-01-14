35d52343e07ee3eda724fa87188542e92eba5df6
ProVitSer <prokinvs@gmail.com>
2023-01-14 20:30:20 +0300

    new: util data to string

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -87,4 +87,24 @@ export class UtilsService {
     }
     return newObj;
   }
+
+  static dataToString<T>(data: T): string | undefined {
+    if (Array.isArray(data)) {
+      return data.toString();
+    }
+    switch (typeof data) {
+      case 'string':
+        return data;
+      case 'number':
+      case 'symbol':
+      case 'bigint':
+      case 'boolean':
+      case 'function':
+        return data.toString();
+      case 'object':
+        return JSON.stringify(data);
+      default:
+        return undefined;
+    }
+  }
 }
