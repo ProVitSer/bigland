ff2f1953917e0e6e5ffbe1a1878e16e1f882111d
ProVitSer <prokinvs@gmail.com>
2023-02-28 22:27:35 +0300

    Fix: выгрузка не обработаных вызовов вечером

--- src/cdr/schedule/cdr.schedule.ts
+++ src/cdr/schedule/cdr.schedule.ts
@@ -1,8 +1,9 @@
+import { DEFAULT_TIMEOUT_HANDLER } from '@app/asterisk/asterisk.config';
 import { LogService } from '@app/log/log.service';
 import { UtilsService } from '@app/utils/utils.service';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Cron, CronExpression, Timeout } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { Model } from 'mongoose';
 import { Cdr, CdrDocument } from '../cdr.schema';
 import { CdrService } from '../cdr.service';
@@ -16,13 +17,13 @@ export class CdrSchedule {
     private readonly log: LogService,
   ) {}
 
-  @Cron(CronExpression.EVERY_10_MINUTES)
-  async updateLdsUserStatus() {
+  @Cron(CronExpression.EVERY_DAY_AT_9PM)
+  async updateNoCompleteCall() {
     try {
       const noSendCdr = await this.cdrModel.find({ complete: { $ne: true }, disposition: { $ne: Disposition.failed } });
       if (noSendCdr.length == 0) return;
       for (const cdr of noSendCdr) {
-        await UtilsService.sleep(2000);
+        await UtilsService.sleep(DEFAULT_TIMEOUT_HANDLER);
         await this.cdrService.sendCdrInfo(cdr);
       }
     } catch (e) {
