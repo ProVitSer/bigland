3c661c439cf4ee056db511426c11ef8b3486e3cb
ProVitSer <prokinvs@gmail.com>
2023-03-01 22:44:48 +0300

    ref amocrm

--- src/health/health-indicators/amocrm.healthindicator.ts
+++ src/health/health-indicators/amocrm.healthindicator.ts
@@ -1,18 +1,18 @@
-import { AmocrmConnector } from '@app/amocrm/amocrm.connect';
-import { AmocrmAPI } from '@app/amocrm/interfaces/amocrm.enum';
+import { AmocrmV4Connector } from '@app/amocrm/v4/amocrm-v4.connect';
+import { AmocrmAPIV4 } from '@app/amocrm/interfaces/amocrm.enum';
 import { Injectable } from '@nestjs/common';
 import { HealthCheckError, HealthIndicator, HealthIndicatorResult } from '@nestjs/terminus';
 import { AMOCRM_PROBLEM } from '../health.constants';
 
 @Injectable()
 export class AmocrmHealthIndicator extends HealthIndicator {
-  constructor(private readonly amocrmConnect: AmocrmConnector) {
+  constructor(private readonly amocrmConnect: AmocrmV4Connector) {
     super();
   }
   public async ping(key: string): Promise<HealthIndicatorResult> {
     try {
       const amocrmClient = await this.amocrmConnect.getAmocrmClient();
-      const response = await amocrmClient.request.get(AmocrmAPI.account);
+      const response = await amocrmClient.request.get(AmocrmAPIV4.account);
       if (!response.data.hasOwnProperty('id')) {
         throw AMOCRM_PROBLEM;
       }
