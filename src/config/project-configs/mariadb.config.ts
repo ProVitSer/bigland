5468bc210ed3d4c7314a3631434d538b8fa5b214
ProVitSer <prokinvs@gmail.com>
2023-03-05 18:22:11 +0300

    pool connect

--- src/config/project-configs/mariadb.config.ts
+++ src/config/project-configs/mariadb.config.ts
@@ -18,6 +18,12 @@ const getMariadbConfig = (configService: ConfigService): SequelizeModuleOptions 
     username,
     password,
     database,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   };
 };
 
