131c2580cf193e360e425db48bfb7871cbcad036
ProVitSer <prokinvs@gmail.com>
2023-03-13 21:44:07 +0300

    Отключили Nack и небольшой ref

--- src/cdr/cdr.subscribers.ts
+++ src/cdr/cdr.subscribers.ts
@@ -5,33 +5,29 @@ import { CdrService } from './cdr.service';
 import { UtilsService } from '@app/utils/utils.service';
 import { QueueTypes } from './interfaces/cdr.enum';
 import { DEFAULT_TIMEOUT } from './cdr.config';
-import { LogService } from '@app/log/log.service';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CdrPubSubInfo } from './interfaces/cdr.interfaces';
 
 @Injectable()
 export class CdrMessagingService {
-  constructor(
-    @InjectModel(Cdr.name) private cdrModel: Model<CdrDocument>,
-    private readonly cdrService: CdrService,
-    private readonly log: LogService,
-  ) {}
+  constructor(@InjectModel(Cdr.name) private cdrModel: Model<CdrDocument>, private readonly cdrService: CdrService) {}
+
   @RabbitSubscribe({
     exchange: 'presence',
     queue: QueueTypes.cdr,
   })
-  public async pubSubHandler(msg: CdrPubSubInfo): Promise<void | Nack> {
+  public async pubSubHandler(msg: CdrPubSubInfo): Promise<void> {
     try {
       await UtilsService.sleep(DEFAULT_TIMEOUT);
       if (await this.checkComplete(msg)) return;
       await this.cdrService.sendCdrInfo(msg.data as Cdr);
     } catch (e) {
-      return new Nack(true);
+      return; //new Nack(true);
     }
   }
 
-  private async checkComplete(msg: CdrPubSubInfo) {
+  private async checkComplete(msg: CdrPubSubInfo): Promise<boolean> {
     const cdr = await this.cdrModel.find({ unicueid: msg.data.unicueid });
     return cdr[0].complete;
   }
