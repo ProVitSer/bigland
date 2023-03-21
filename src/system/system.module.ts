1ae2c99ba75c9508752884590af018f5c8517a60
ProVitSer <prokinvs@gmail.com>
2023-03-21 21:41:45 +0300

    Отключаем GSM

--- src/system/system.module.ts
+++ src/system/system.module.ts
@@ -1,20 +1,13 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { System, SystemSchema } from './system.schema';
 import { SystemService } from './system.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UpdateSystemConfigSchedule } from './schedule/update-system-config-schedule';
 import { ScheduleModule } from '@nestjs/schedule';
 import { LogModule } from '@app/log/log.module';
-import { GsmGatewayApiModule } from '@app/gsm-gateway-api/gsm-gateway-api.module';
 
 @Module({
-  imports: [
-    ScheduleModule.forRoot(),
-    LogModule,
-    forwardRef(() => GsmGatewayApiModule),
-    MongooseModule.forFeature([{ name: System.name, schema: SystemSchema }]),
-  ],
-  providers: [SystemService, System, UpdateSystemConfigSchedule],
+  imports: [ScheduleModule.forRoot(), LogModule, MongooseModule.forFeature([{ name: System.name, schema: SystemSchema }])],
+  providers: [SystemService, System],
   exports: [System, SystemService],
 })
 export class SystemModule {}
