083c7964e9b4aa6a2097e4fde3d9c358d8488e48
ProVitSer <prokinvs@gmail.com>
2023-04-29 22:20:45 +0300

    ref\feat: update amocrmUser schedule\controller

--- src/amocrm-users/amocrm-users.service.ts
+++ src/amocrm-users/amocrm-users.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AmocrmUsers, AmocrmUsersDocument } from './amocrm-users.schema';
+import { LdsService } from '@app/lds/lds.service';
+import { Lds } from '@app/lds/lds.schema';
+import * as moment from 'moment';
+import { DATE_FORMAT } from './amocrm-users.constants';
 
 @Injectable()
 export class AmocrmUsersService {
   constructor(
     @InjectModel(AmocrmUsers.name)
     private amocrmUsersModel: Model<AmocrmUsersDocument>,
+    private readonly ldsService: LdsService,
   ) {}
 
   public async getAmocrmUser(extension: string): Promise<AmocrmUsers[]> {
@@ -26,7 +31,7 @@ export class AmocrmUsersService {
     }
   }
 
-  public async clearCollection(): Promise<void> {
+  private async clearCollection(): Promise<void> {
     try {
       await this.amocrmUsersModel.deleteMany({});
     } catch (e) {
@@ -47,5 +52,39 @@ export class AmocrmUsersService {
     } catch (e) {
       throw e;
     }
+  }
+
+  public async updateAmocrmUsers() {
+    try {
+      const lds = await this.ldsService.getLdsUser();
+      if (this.checkNeedUpdate(lds)) {
+        await this.clearCollection();
+        const formatLds = this.formatLdsUsers(lds);
+        await this.addUsers(formatLds);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  private checkNeedUpdate(ldsUsers: Lds[]): boolean {
+    const now = moment().format(DATE_FORMAT);
+    const ldsUpdateTime = moment(ldsUsers[0].changed).format(DATE_FORMAT);
+    console.log(now);
+    console.log(ldsUpdateTime);
+    return now == ldsUpdateTime;
+  }
+
+  private formatLdsUsers(ldsUsers: Lds[]): AmocrmUsers[] {
+    const formatLdsUsers: AmocrmUsers[] = [];
+    ldsUsers.map((ldsUser: Lds) => {
+      if (ldsUser.sip_id !== null) {
+        formatLdsUsers.push({
+          localExtension: ldsUser.sip_id,
+          amocrmId: ldsUser.amo[0].amo_id,
+        });
+      }
+    });
+    return formatLdsUsers;
   }
 }
