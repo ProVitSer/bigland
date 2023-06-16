a5ce07d2238ef466a30dad4a1029b52c1c51bdb8
ProVitSer <prokinvs@gmail.com>
2023-06-16 17:23:45 +0300

    feat: методы работы с файлами

--- src/files-api/files-api.module.ts
+++ src/files-api/files-api.module.ts
@@ -3,9 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { FilesCreateService } from './files-create/files-create.service';
 import { FileUtilsService } from './file-utils/file-utils.service';
 import { FilesMergeService } from './files-merge/files-merge.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Files, FilesSchema } from './files.schema';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, MongooseModule.forFeature([{ name: Files.name, schema: FilesSchema }])],
   providers: [FilesCreateService, FileUtilsService, FilesMergeService],
   exports: [FilesCreateService, FileUtilsService, FilesMergeService],
 })
---
+++ src/files-api/interfaces/files.enum.ts
@@ -0,0 +1,18 @@
+export type FileType = Blob | Buffer | string;
+
+export enum FileFormatType {
+  pdf = 'pdf',
+  jpg = 'jpg',
+  jpeg = 'jpeg',
+  png = 'png',
+  doc = 'doc',
+  docx = 'docx',
+  tif = 'tif',
+  xls = 'xls',
+  xlsx = 'xlsx',
+  csv = 'csv',
+  odt = 'odt',
+  ods = 'ods',
+  buffer = 'buffer',
+  txt = 'txt',
+}
