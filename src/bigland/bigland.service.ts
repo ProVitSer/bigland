ec11d4feaadc455b4df981e9b63d378123568f3d
ProVitSer <prokinvs@gmail.com>
2023-07-04 01:24:39 +0300

    feat: Observable fun for check api result

--- src/bigland/bigland.service.ts
+++ src/bigland/bigland.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { DefaultApplicationApiStruct } from './interfaces/bigland.interfaces';
 import { ApplicationApiActionStatus } from './interfaces/bigland.enum';
 import { UtilsService } from '@app/utils/utils.service';
+import { distinctUntilChanged } from 'rxjs';
 
 @Injectable()
 export class BiglandService {
@@ -13,5 +14,39 @@ export class BiglandService {
       applicationId: UtilsService.generateId(true),
       status: ApplicationApiActionStatus.inProgress,
     };
+  }
+
+  public async subscribeApiResult<T extends { status: ApplicationApiActionStatus }>(fn: () => Promise<any>, timeout: number): Promise<T> {
+    return new Promise((resolve, reject) => {
+      const subscriber = UtilsService.getObservableFn(fn, timeout)
+        .pipe(
+          distinctUntilChanged((prev: T, current: T) => {
+            return prev.status === current.status;
+          }),
+        )
+        .subscribe({
+          next: async (res: T) => {
+            switch (res.status) {
+              case ApplicationApiActionStatus.completed:
+                subscriber.unsubscribe();
+                resolve(res);
+                break;
+              case ApplicationApiActionStatus.inProgress:
+                break;
+              case ApplicationApiActionStatus.apiFail:
+                subscriber.unsubscribe();
+                reject(res);
+                break;
+              case ApplicationApiActionStatus.cancel:
+                subscriber.unsubscribe();
+                break;
+            }
+          },
+          error: (e: unknown) => {
+            subscriber.unsubscribe();
+            reject(e);
+          },
+        });
+    });
   }
 }
