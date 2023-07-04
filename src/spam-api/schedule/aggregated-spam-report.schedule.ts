3fbe437ba5bb8bf44e24f017d5cdc63cce02d6e5
ProVitSer <prokinvs@gmail.com>
2023-07-04 22:34:40 +0300

    ref: отключаем отчет по расписанию

--- src/spam-api/schedule/aggregated-spam-report.schedule.ts
+++ src/spam-api/schedule/aggregated-spam-report.schedule.ts
@@ -1,6 +1,6 @@
 import { LogService } from '@app/log/log.service';
 import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+// import { Cron, CronExpression } from '@nestjs/schedule';
 import { AggregatedSpamService } from '../services/aggregate-spam.service';
 
 @Injectable()
@@ -9,7 +9,7 @@ export class AggregatedSpamReportSchedule {
 
   constructor(private readonly log: LogService, private readonly aggregatedSpamService: AggregatedSpamService) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_8PM)
+  //@Cron(CronExpression.EVERY_DAY_AT_8PM)
   async startAggregateReport() {
     try {
       await this.aggregatedSpamService.aggregateScheduleReport();
