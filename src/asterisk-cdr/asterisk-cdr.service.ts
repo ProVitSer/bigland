ed4c89d3ec4b4f99b35ba4038bf60c66c7e8c3b6
ProVitSer <prokinvs@gmail.com>
2023-08-10 18:37:54 +0300

    feat: костыль по обработке/поиску звонков Pozvonim

--- src/asterisk-cdr/asterisk-cdr.service.ts
+++ src/asterisk-cdr/asterisk-cdr.service.ts
@@ -20,6 +20,10 @@ export class AsteriskCdrService {
 
   public async searchIncomingCallInfoInCdr(uniqueid: string): Promise<AsteriskCdr[]> {
     try {
+      // Костыль по вычленению переведенных вызовов через Позвоним
+      const pattern = /^.*;\d$/;
+      if (pattern.test(uniqueid)) return this.searchTransferPozvonimIncomingCallInfoInCdr(uniqueid);
+
       this.log.info(`Входящий вызов ${uniqueid}`, AsteriskCdrService.name);
       const result = await this.getCallInfo.findAll({
         raw: true,
@@ -27,6 +31,34 @@ export class AsteriskCdrService {
         where: {
           uniqueid: {
             [Op.like]: uniqueid,
+          },
+        },
+        order: [['billsec', 'DESC']],
+      });
+
+      this.log.info(result, AsteriskCdrService.name);
+      return result;
+    } catch (e) {
+      this.log.error(`${ASTERISK_CDR_INCOMING_CALL_ERROR}: ${e}`, AsteriskCdrService.name);
+      return;
+    }
+  }
+
+  public async searchTransferPozvonimIncomingCallInfoInCdr(uniqueid: string): Promise<AsteriskCdr[]> {
+    try {
+      const formatUniqueid = uniqueid.includes(';2') ? uniqueid.replace(';2', '') : uniqueid;
+      const newUniqueid = formatUniqueid.substring(0, formatUniqueid.length - 5);
+
+      this.log.info(`Переадресованный вызов с Pozvonim ${uniqueid}`, AsteriskCdrService.name);
+      const result = await this.getCallInfo.findAll({
+        raw: true,
+        attributes: CDR_ATTRIBUTES,
+        where: {
+          uniqueid: {
+            [Op.like]: `${newUniqueid}%`,
+          },
+          dcontext: {
+            [Op.like]: 'ext-local',
           },
         },
         order: [['billsec', 'DESC']],
@@ -81,9 +113,13 @@ export class AsteriskCdrService {
           },
         },
       });
-
-      this.log.info(result, AsteriskCdrService.name);
-      return result;
+      // Небольшой костыл, меняем channel с локальным каналом "Local/124997@from-internal-additional-00002a89;1" на реальный канал пользователя "PJSIP/790-0012ec3b"
+      const updateResult = result.map((c: AsteriskCdr) => {
+        const newChannel = `PJSIP/${c.cnum}-0012ec3b`;
+        return { ...c, channel: newChannel };
+      }) as AsteriskCdr[];
+      this.log.info(updateResult, AsteriskCdrService.name);
+      return updateResult;
     } catch (e) {
       this.log.error(`${ASTERISK_CDR_POZVONIM_CALL_ERROR}: ${e}`, AsteriskCdrService.name);
       return;
