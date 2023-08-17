01374857f3d86de139e435086cedc707979fd1b1
ProVitSer <prokinvs@gmail.com>
2023-08-17 16:17:55 +0300

    fix: add call timeout, ref originate info

--- src/asterisk/ari/adapters/pozvonim-call.adapter.ts
+++ src/asterisk/ari/adapters/pozvonim-call.adapter.ts
@@ -6,7 +6,12 @@ import { Injectable } from '@nestjs/common';
 import { AsteriskContext, ChannelType } from '../../interfaces/asterisk.enum';
 import { AsteriskAriOriginate } from '../../interfaces/asterisk.interfaces';
 import { NumbersInfo } from '@app/operators/operators.schema';
-import { POZVONIM_CALL_LOCAL_PREFIX, POZVONIM_CALL_CC_PREFIX } from '../ari.constants';
+import {
+  POZVONIM_CALL_LOCAL_PREFIX,
+  POZVONIM_CALL_CC_PREFIX,
+  POZVONIM_LOCAL_EXTENSION_TIMEOUT,
+  POZVONIM_GROUP_TIMEOUT,
+} from '../ari.constants';
 
 @Injectable()
 export class PozvonimCallDataAdapter {
@@ -15,31 +20,22 @@ export class PozvonimCallDataAdapter {
   constructor(private readonly operatorsService: OperatorsService) {}
 
   public async getLocalExtensionOriginateInfo(data: PozvominCall): Promise<AsteriskAriOriginate> {
-    const info = await this.getOperatorInfo(data);
-    return {
-      endpoint: `${ChannelType.LOCAL}/${POZVONIM_CALL_LOCAL_PREFIX}${data.SIP_ID}@${AsteriskContext.fromInternalAdditional}`,
-      callerId: info.dstNumber,
-      context: AsteriskContext.pozvonim,
-      extension: data.SIP_ID,
-      variables: {
-        dstNumber: info.dstNumber,
-        accountcode: this.accountcode,
-        localExtension: data.SIP_ID,
-        outSuffix: info.numberInfo.outSuffix,
-        trunkCIDOverride: info.callerId,
-        pbxTrunkNumber: info.numberInfo.pbxTrunkNumber,
-      },
-    };
+    return await this.getOriginateInfo(data, POZVONIM_CALL_LOCAL_PREFIX, POZVONIM_LOCAL_EXTENSION_TIMEOUT);
   }
 
   public async getCCOriginateInfo(data: PozvominCall): Promise<AsteriskAriOriginate> {
+    return await this.getOriginateInfo(data, POZVONIM_CALL_CC_PREFIX, POZVONIM_GROUP_TIMEOUT);
+  }
+
+  private async getOriginateInfo(data: PozvominCall, prifix: string, timeout: number) {
     const info = await this.getOperatorInfo(data);
 
     return {
-      endpoint: `${ChannelType.LOCAL}/${POZVONIM_CALL_CC_PREFIX}${data.SIP_ID}@${AsteriskContext.fromInternalAdditional}`,
+      endpoint: `${ChannelType.LOCAL}/${prifix}${data.SIP_ID}@${AsteriskContext.fromInternalAdditional}`,
       callerId: info.dstNumber,
       context: AsteriskContext.pozvonim,
       extension: data.SIP_ID,
+      timeout,
       variables: {
         dstNumber: info.dstNumber,
         accountcode: this.accountcode,
