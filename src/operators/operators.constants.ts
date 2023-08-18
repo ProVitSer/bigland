c1d6bc0c648dd596163aa6e692fe69f00112a471
ProVitSer <prokinvs@gmail.com>
2023-08-19 00:02:29 +0300

    feat: addn default operators settings

--- src/operators/operators.constants.ts
+++ src/operators/operators.constants.ts
@@ -2,10 +2,25 @@ import { OperatorsName } from './interfaces/operators.enum';
 
 export const OPERATOR_PROJ = { _id: 0, name: 1, 'numbers.callerId': 1 };
 
-export const OPERATOR_DEFAULT_SETTINGS: { [key in OperatorsName]?: { [key: string]: any } } = {
+export const OPERATOR_DEFAULT_SETTINGS: { [key in OperatorsName]: { [key: string]: any } } = {
   [OperatorsName.mango]: {
     pbxTrunkNumber: '2',
     outSuffix: 'Mango',
-    isActive: true,
+  },
+  [OperatorsName.zadarma]: {
+    pbxTrunkNumber: '22',
+    outSuffix: 'Zadarma-IP',
+  },
+  [OperatorsName.optima]: {
+    pbxTrunkNumber: '4',
+    outSuffix: 'Optima-XXXXXXXX',
+  },
+  [OperatorsName.mtt]: {
+    pbxTrunkNumber: '21',
+    outSuffix: 'MTT',
+  },
+  [OperatorsName.beeline]: {
+    pbxTrunkNumber: '20',
+    outSuffix: 'Beeline',
   },
 };
