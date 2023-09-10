5e32f94e88cb51d31f3c70b9c9a849d2f20b09af
ProVitSer <prokinvs@gmail.com>
2023-09-10 16:51:41 +0300

    delno used const

--- src/asterisk/ari/ari.constants.ts
+++ src/asterisk/ari/ari.constants.ts
@@ -7,7 +7,6 @@ export const CONTINUE_DIALPLAN_BLACKLIST_ERROR = 'Ошибка маршрути�
 export const CONTINUE_DIALPLAN_CHANSPY_ERROR = 'Ошибка маршрутизации вызова по стандартному маршруту через CHANSPY';
 export const PLAYBACK_ERROR = 'Ошибка обработки вызова ChanSpy Playback';
 export const NUMBER_IN_BLACK_LIST = 'Вызов по входящему вызову попал в black-list';
-export const NUMBER_FORMAT = 10;
 export const DEFAULT_LOCAL_EXTENSION = '999';
 export const AMI_OUTBOUND_CALL = {
   context: AsteriskContext.fromInternal,
